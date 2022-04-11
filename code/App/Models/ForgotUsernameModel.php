510caf664ab9cfcfbf18e2717311b41aa445040b
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-04-11 22:31:22 +0200

    forgot_username

---
+++ code/App/Models/ForgotUsernameModel.php
@@ -0,0 +1,45 @@
+<?php
+
+    namespace App\Models;
+
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
+
+
+    class ForgotUsernameModel 
+        extends Model
+    {
+        use HasFactory;
+        public const DB_TABLE_NAME = 'forgot_username';
+        
+        protected $primaryKey = 'id';
+
+        protected $fillable =
+        [
+            'email_id',
+            'token',
+            'created_at',
+            'uuid',
+            'mailed_at',
+            'accessed_at'
+        ];
+
+
+        protected $hidden =
+        [
+            'created_at',
+            'mailed_at',
+            'accessed_at'
+        ];
+
+
+        protected $casts =
+        [
+            'created_at' => 'datetime',
+            'mailed_at' => 'datetime',
+            'accessed_at' => 'datetime'
+        ];
+
+    }
+
+?>
